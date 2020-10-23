851b5503ab5ad2f9ca4279a292207e518cb4b016
jfourdachon <jfourdachon@yahoo.com>
2020-10-23 12:39:58 +0200

    WIP- rich editor

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{ src: '~/plugins/vue-editor.js', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
--- pages/quizz-2.vue
+++ pages/quizz-2.vue
@@ -4,40 +4,41 @@
     <b-row class="pl-3 mb-4" align-v="center">
       Exemple: Voici ce que vous
       <b-form-select
-        v-model="selected"
-        :options="options1"
+        :options="exemple1"
         class="exemple-select mx-2"
       ></b-form-select>
       réaliser avec ce
       <b-form-select
-        v-model="selected"
-        :options="options2"
+        :options="exemple2"
         class="exemple-select ml-2"
       ></b-form-select>
     </b-row>
     <h5>Entrez le résultat final dans le formulaire ci dessous</h5>
-    <label for="textarea"
+    <label for="editor"
       >Note: Placez les mots clés(qui seront dans les trous) entre crochets, ex:
       les règles de [sécurité]</label
     >
     <b-form-textarea
-      id="textarea"
+      id="editor"
       v-model="text"
       placeholder="Votre texte ici..."
       rows="15"
       max-rows="15"
     ></b-form-textarea>
+    <client-only>
+      <vue-editor v-model="text" :editorOptions="customToolbar"></vue-editor>
+    </client-only>
     <b-button variant="outline-success" class="mt-3" @click="handleKeyWords"
       >Suivant</b-button
     >
-    <div class="mt-4">
+    <div class="mt-4" v-if="step2 === true">
       <p>Pour chaque mot clé, renseignez 2 mauvaises réponses</p>
       <b-table striped hover :items="keyWordsTable" :fields="fields">
-        <template v-slot:cell(Mauvaise_reponse_1)="row">
-          <b-form-input v-model="row.item.Mauvaise_reponse_1" />
+        <template v-slot:cell(badAnswer1)="row">
+          <b-form-input v-model="row.item.badAnswer1" />
         </template>
-        <template v-slot:cell(Mauvaise_reponse_2)="row">
-          <b-form-input v-model="row.item.Mauvaise_reponse_2" />
+        <template v-slot:cell(badAnswer2)="row">
+          <b-form-input v-model="row.item.badAnswer2" />
         </template>
       </b-table>
       <b-button
@@ -59,41 +60,50 @@ export default {
       keyWordsTable: [],
       step2: false,
       fields: [
-        {
-          Bonne_Réponse: " Bonne_Réponse",
-          Mauvaise_reponse_1: " Mauvaise_reponse_1",
-          Mauvaise_reponse_2: " Mauvaise_reponse_2"
-        }
+        { goodAnswer: "Bonne Réponse" },
+        { badAnswer1: "Mauvaise Réponse 1" },
+        { badAnswer2: "Mauvaise Réponse 2" },
       ],
-      options1: [
+      exemple1: [
         { value: "", text: "pouvez" },
-        { value: "", text: "pourrez" }
+        { value: "", text: "pourrez" },
       ],
-      options2: [
+      exemple2: [
         { value: "", text: "quizz" },
         { value: "", text: "module" },
-        { value: "", text: "exercice" }
-      ]
+        { value: "", text: "exercice" },
+      ],
+      customToolbar: {
+        modules: {
+          toolbar: false,
+        },
+      },
     };
   },
   methods: {
-    handleKeyWords(e) {
-      e.preventDefault();
+    putResulstInColor(txt, elem) {
+      txt.replace(elem);
+    },
+    handleKeyWords() {
       const regex = /\[.*?\]/g;
-      const match = this.text.match(regex);
-      const keyWords = match.map(el => {
+      const match = this.text.match(this.regex);
+      const keyWords = match.map((el) => {
+        console.log({ el });
         this.keyWordsTable.push({
-          Bonne_réponse: el.slice(1, -1),
-          Mauvaise_reponse_1: "",
-          Mauvaise_reponse_2: ""
+          goodAnswer: el.slice(1, -1),
+          badAnswer1: "",
+          badAnswer2: "",
         });
       });
+      this.step2 = true;
+      console.log(this.keyWordsTable);
     },
     handleEditAnswers(e) {
       e.preventDefault();
-      console.log(this.keyWordsTable);
-    }
-  }
+      console.log(this.keyWordsTable[0].goodAnswer);
+    },
+  },
+  mounted() {},
 };
 </script>
 
